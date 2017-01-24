d185d65d0615ff7a2c51040e1f3ce39407c4e6bb
Nazariy995 <nazariy1995@gmail.com>
2017-01-24 01:17:06 -0500

    CHNG: temporarily removed user object

--- src/main/resources/static/views/student/home/controller.js
+++ src/main/resources/static/views/student/home/controller.js
@@ -4,7 +4,6 @@ function Controller($scope, $state, SessionService){
     this._$state = $state;
     this._$scope = $scope;
     this._$scope.courses = [];
-    this.user =
 };
 
 module.exports = angular.module('app.views.student.home.controller', [])
