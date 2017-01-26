2d0553bea7c153e570fb2ebf37a5fb4435ca5765
Nazariy995 <nazariy1995@gmail.com>
2017-01-26 00:47:57 -0500

    ADD: navigation links for Student

--- src/main/resources/static/app_settings.js
+++ src/main/resources/static/app_settings.js
@@ -2,5 +2,19 @@ module.exports = angular.module('app.settings', [])
 .constant("appSettings", {
     "API" : {
         "basePath" : "http://localhost:8080/rest"
+    },
+    "STUDENT" : {
+        "mainNavigationLinks" : [
+            {
+                "name" : "Courses",
+                "url" : "home/student",
+                "state" : "home.student"
+            },
+            {
+                "name" : "Account",
+                "url" : "home/account",
+                "state" : "home.account"
+            }
+        ]
     }
 })
