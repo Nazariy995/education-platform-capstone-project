41893241649e3eb1d18df626cda826057d569b0e
Nazariy995 <nazariy1995@gmail.com>
2017-01-26 12:39:20 -0500

    ADD: Navigation links to the home page based on User Role

--- src/main/resources/static/views/app/home/controller.js
+++ src/main/resources/static/views/app/home/controller.js
@@ -1,25 +1,28 @@
 
-function Controller($scope, $state, SessionService) {
+function Controller($scope, $state, SessionService, appSettings) {
     "ngInject";
 
-    var self = this;
-    self._$scope = $scope;
-    self._$scope.navigation_links = [];
-//    self.user_roles = SessionService.getUser().roles;
-//    $state.go('home.student');
-
-//
-
-
-//    if(self.user_roles.indexOf("USER") != -1){
-//        console.log("yes");
-//        $state.go("home.teacher");
-//    }
+    this._$scope = $scope;
+    this.navigationLinks = [];
+    this._appSettings = appSettings;
+    this._SessionService = SessionService;
+    this.init();
 
 }
 
+Controller.prototype.init = function(){
+    var self = this;
+    var roles = self._SessionService.getUser().roles;
+    if (roles.indexOf("USER") != -1){
+        var role = "USER";
+        self.navigationLinks = self._appSettings[role]["mainNavigationLinks"];
+    }
+}
+
+
 
 module.exports = angular.module('app.views.app.controller', [
-    "app.components.services.session_service"
+    "app.components.services.session_service",
+    "app.settings"
 ])
 .controller('HomeCtrl', Controller);
