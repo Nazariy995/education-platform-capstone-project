eae2dd7cc263593134624a975ebd528dd95a8208
Nazariy995 <nazariy1995@gmail.com>
2017-01-29 23:33:39 -0500

    ADD: Assignments model to retrieve assignments based on courseID, Assignment controller with the view

---
+++ src/main/js/models/assignment.js
@@ -0,0 +1,23 @@
+/*
+Description: Add, Get, Set, Delete Assignments
+*/
+
+function AssignmentService($http, appSettings){
+    "ngInject";
+
+    this._$http = $http;
+    this._appSettings = appSettings;
+
+}
+
+AssignmentService.prototype.getAssignments = function(courseId){
+    return this._$http
+          .get(this._appSettings.API.basePath + '/rest/student/course/' + courseId + '/modules')
+          .then(function (res) {
+            return res.data;
+          });
+}
+
+module.exports = angular.module('app.models.assignment', [
+    'app.settings'
+]).service('AssignmentService', AssignmentService);
--- src/main/js/views/student/assignment/controller.js
+++ src/main/js/views/student/assignment/controller.js
@@ -1,7 +1,19 @@
-var app = angular.module('hello');
-app.controller('Student.AssCtrl', function($rootScope, $scope,$stateParams){
-    $scope.courses = [];
 
+function Controller($scope, $state, $stateParams, AssignmentService){
+    "ngInject";
 
+    this.pageName = "Assignments";
+    this._AssignmentService = AssignmentService;
+    console.log($stateParams);
+    this.init();
 
-});
+};
+
+Controller.prototype.init = function(){
+    var self = this;
+}
+
+module.exports = angular.module('app.views.student.assignments.controller', [
+    'app.models.assignment'
+])
+.controller('Student.AssignmentCtrl', Controller);
