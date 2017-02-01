553ef4bed3e9beeee17ee4901161440f7f629bf9
Nazariy995 <nazariy1995@gmail.com>
2017-02-01 11:34:36 -0500

    CHNG: Using UI Router to the full advantage together with structureal changes to allow for multiple navigations

--- src/main/js/app_router.js
+++ src/main/js/app_router.js
@@ -5,48 +5,101 @@ function Router($stateProvider, $httpProvider, $locationProvider){
 
     var states = [
         {
-            name:'login',
-            url:'/',
-            templateUrl:'views/app/login/login.html',
-            controller: 'LoginCtrl',
-            controllerAs: 'login'
+            name : 'app',
+            url : '/',
+            views : {
+                'app' : {
+                    templateUrl : 'views/app/home/home.html'
+                },
+                'mainNavigation@app' : {
+                    templateUrl : 'views/app/mainNavigation/home.html'
+                }
+                
+            }
         },
         {
-            name:'home',
-            url:'/home',
-            templateUrl:'views/app/home/home.html',
-            controller: 'HomeCtrl',
-            controllerAs: 'home'
+            name : 'app.login',
+            url : 'login',
+            views : {
+                'app@' : {
+                    templateUrl : 'views/app/login/login.html',
+                    controller: 'LoginCtrl',
+                    controllerAs: 'login'
+                }
+            }
         },
         {
-            name:'home.teacher',
-            url:'/teacher',
-            templateUrl:'views/teacher/home/home.html',
-            controller: 'Teacher.HomeCtrl',
-            controllerAs: 'teacherHome'
-        },
+            name : 'app.account',
+            url : 'account',
+            views : {
+                'mainContent@app' : {
+                    templateUrl : 'views/app/account/account.html',
+                    controller : 'AccountCtrl',
+                    controllerAs : 'account'  
+                }
+            }
+        }, 
         {
-            name:'home.student',
-            url:'/student',
-            templateUrl:'views/student/home/home.html',
-            controller: 'Student.HomeCtrl',
-            controllerAs: 'studentHome'
-        },
+            name : 'app.courses',
+            url : 'courses',
+            views : {
+                'mainContent@app' : {
+                    templateUrl: 'views/student/home/home.html',
+                    controller: 'Student.HomeCtrl',
+                    controllerAs: 'studentHome' 
+                }
+            }
+        }, 
         {
-            name : 'home.account',
-            url : '/account',
-            templateUrl : 'views/app/account/account.html',
-            controller : 'AccountCtrl',
-            controllerAs : 'account'
-        },
-        {
-            name : 'home.assignments',
-            url :  "/course/{courseId}/assignments",
-            templateUrl : 'views/student/assignment/home.html',
-            controller : 'Student.AssignmentCtrl',
-            controllerAs : 'courseAssignments'
-
+            name : 'app.course',
+            url  : 'courses/{courseId}',
+            views : {
+                'mainContent@app' : {
+                    templateUrl : 'views/student/course/home.html'
+                },
+                'childNavigation@app.course' : {
+                    templateUrl : 'views/student/child_navigation/home.html'
+                }
+            }
+            
         }
+        
+//        {
+//            name:'home',
+//            url:'/home',
+//            templateUrl:'views/app/home/home.html',
+//            controller: 'HomeCtrl',
+//            controllerAs: 'home'
+//        },
+//        {
+//            name:'home.teacher',
+//            url:'/teacher',
+//            templateUrl:'views/teacher/home/home.html',
+//            controller: 'Teacher.HomeCtrl',
+//            controllerAs: 'teacherHome'
+//        },
+//        {
+//            name:'home.student',
+//            url:'/student',
+//            templateUrl:'views/student/home/home.html',
+//            controller: 'Student.HomeCtrl',
+//            controllerAs: 'studentHome'
+//        },
+//        {
+//            name : 'home.account',
+//            url : '/account',
+//            templateUrl : 'views/app/account/account.html',
+//            controller : 'AccountCtrl',
+//            controllerAs : 'account'
+//        },
+//        {
+//            name : 'home.assignments',
+//            url :  "/course/{courseId}/assignments",
+//            templateUrl : 'views/student/assignment/home.html',
+//            controller : 'Student.AssignmentCtrl',
+//            controllerAs : 'courseAssignments'
+//
+//        }
     ]
 
     states.forEach(function(state) {
