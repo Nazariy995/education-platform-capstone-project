d3d0cc059264bdffb2c27651e9e5019caff98f78
Nazariy995 <nazariy1995@gmail.com>
2017-02-23 00:51:12 -0500

    ADD: Question model to get the data for the questions

--- src/main/js/models/module.js
+++ src/main/js/models/module.js
@@ -3,9 +3,11 @@
 var courseModel = require('./course');
 var assignmentModel = require('./assignment');
 var groupModel = require('./group');
+var questionModel = require('./question');
 
 module.exports = angular.module('app.models', [
     courseModel.name,
     assignmentModel.name,
-    groupModel.name
+    groupModel.name,
+    questionModel.name
 ])
---
+++ src/main/js/models/question.js
@@ -0,0 +1,47 @@
+/*
+Description: Add, Get, Post questions to assignments
+*/
+
+function Service($http, appSettings, SessionService){
+    "ngInject";
+    var self = this;
+    this._$http = $http;
+    this._appSettings = appSettings;
+    this._SessionService = SessionService;
+    this.courseUserIdKey = appSettings.API.PARAMS.courseUserId;
+    this.init();
+}
+
+Service.prototype.init = function(){
+    var self = this;
+    self.getConfig();
+};
+
+Service.prototype.getConfig = function(){
+    var self = this;
+    self.params = {};
+    self.params[self.courseUserIdKey] = self._SessionService.getCourseUserId();
+    self.config = {
+        params : self.params
+    };
+};
+
+Service.prototype.getQuestions = function(courseId, moduleId, pageNumber){
+    var self = this;
+    self.getConfig();
+    var url = self._appSettings.API.basePath
+    + '/rest/student/course/'+ courseId
+    +'/module/' + moduleId
+    + '?page=' + pageNumber;
+    return this._$http
+          .get(url, self.config)
+          .then(function (res) {
+            return res.data;
+          });
+};
+
+
+
+module.exports = angular.module('app.models.question', [
+    'app.settings'
+]).service('QuestionService', Service);
