a705d54d83f7fdde53aac86c7c54df9cd716c634
Nazariy995 <nazariy1995@gmail.com>
2017-02-28 16:30:18 -0500

    ADD: Free Response directive

---
+++ src/main/js/components/quiz/free_response/directive.js
@@ -0,0 +1,26 @@
+function Directive($state){
+    "ngInject";
+
+    function link(scope, element, attributes){
+
+        scope.model = {
+            type : scope.field.questionType
+        };
+
+    }
+
+    var directive = {
+        controller: 'InputComponentController',
+        controllerAs: 'componentCtrl',
+        templateUrl: 'components/quiz/free_response/home.html',
+        link : link,
+        scope: {
+            model: '=',
+            field: '='
+        }
+    }
+
+    return directive;
+}
+
+module.exports =  Directive;
--- src/main/js/components/quiz/module.js
+++ src/main/js/components/quiz/module.js
@@ -2,9 +2,11 @@
 var controller = require('./controller');
 var multipleChoice = require('./multiple_choice/module');
 var numeric = require('./numeric/module');
+var freeResponse = require('./free_response/module');
 
 module.exports = angular.module('app.components.quiz', [
     multipleChoice.name,
-    numeric.name
+    numeric.name,
+    freeResponse.name
 ])
 .controller("InputComponentController", controller)
