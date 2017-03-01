59ea322e4892ae8c865dd7c92d26787a8d6c1d4f
Nazariy995 <nazariy1995@gmail.com>
2017-03-01 16:39:23 -0500

    ADD: restrict modal to delete elements not accessible to certain roles

--- src/main/js/components/module.js
+++ src/main/js/components/module.js
@@ -3,12 +3,14 @@ var services = require('./services/module');
 var childNavigation = require('./child_navigation/module');
 var mainNavigation = require('./main_navigation/module');
 var quiz = require('./quiz/module');
-var popup_modal = require('./popup_modal/module');
+var popupModal = require('./popup_modal/module');
+var restrictModal = require('./restrict/module');
 
 module.exports = angular.module('app.components', [
     services.name,
     childNavigation.name,
     mainNavigation.name,
-    popup_modal.name,
-    quiz.name
+    popupModal.name,
+    quiz.name,
+    restrictModal.name
 ]);
