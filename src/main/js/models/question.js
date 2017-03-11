71204e2d93e2e2a2798ae6bb7f160622c136d50c
Nazariy995 <nazariy1995@gmail.com>
2017-03-11 17:51:32 -0500

    ADD: get answers endpoint

--- src/main/js/models/question.js
+++ src/main/js/models/question.js
@@ -33,6 +33,7 @@ Service.prototype.getQuestions = function(courseId, moduleId, pageNumber){
     + '/rest/student/course/'+ courseId
     +'/module/' + moduleId
     + '?page=' + pageNumber;
+    self.config.cache = true;
     return this._$http
           .get(url, self.config)
           .then(function (res) {
@@ -51,12 +52,26 @@ Service.prototype.saveAnswers = function(courseId, moduleId, groupId, payload){
         .post(url, payload, self.config)
         .then(function (res) {
             console.log("Save all the answers");
-            console.log("res.data");
+            console.log(res.data);
             return res.data;
         });
 }
 
-
+Service.prototype.getAnswers = function(courseId, moduleId, groupId){
+    var self = this;
+    self.getConfig();
+    var url = self._appSettings.API.basePath + '/rest/student/course/'+
+    courseId+ '/module/'
+    + moduleId + "/group/"
+    + groupId + "/answers/?showSaved=true";
+    return this._$http
+        .get(url, self.config)
+        .then(function (res) {
+            console.log("Get all answers");
+            console.log(res.data);
+            return res.data;
+        });
+};
 
 module.exports = angular.module('app.models.question', [
     'app.settings'
