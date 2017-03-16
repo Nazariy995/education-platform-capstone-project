77407bfeb72ef87e7dfc82199315fa6391ec90ee
Nazariy995 <nazariy1995@gmail.com>
2017-03-15 21:24:20 -0400

    ADD: Function to upload a csv to the endpoint

--- src/main/js/models/course_members.js
+++ src/main/js/models/course_members.js
@@ -2,12 +2,13 @@
 Description: Add, Get, Post course members
 */
 
-function Service($http, appSettings, SessionService){
+function Service($http, appSettings, SessionService, Upload){
     "ngInject";
     var self = this;
     this._$http = $http;
     this._appSettings = appSettings;
     this._SessionService = SessionService;
+    this._Upload = Upload;
     this.courseUserIdKey = appSettings.API.PARAMS.courseUserId;
     this.init();
 }
@@ -40,6 +41,23 @@ Service.prototype.getCourseMembers = function(courseId){
             return res.data;
           });
 };
+
+Service.prototype.addCourseMembers = function(courseId, file){
+    var self = this;
+    self.getConfig();
+    var url = self._appSettings.API.basePath
+    + '/rest/instructor/course/'+ courseId
+    +'/bulk-students';
+    var data = {};
+    data["file"] = file;
+    return self._Upload.upload({url, data })
+          .then(function (res) {
+            console.log("Uploaded Course Members");
+            console.log(res);
+            return res.data;
+          });
+};
+
 
 module.exports = angular.module('app.models.course_members', [
     'app.settings'
