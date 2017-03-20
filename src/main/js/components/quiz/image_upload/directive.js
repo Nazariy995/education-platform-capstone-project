21d1aae27acfa94d854f8afd5bd791d549df40cd
Nazariy995 <nazariy1995@gmail.com>
2017-03-20 12:43:34 -0400

    ADD: comments

--- src/main/js/components/quiz/image_upload/directive.js
+++ src/main/js/components/quiz/image_upload/directive.js
@@ -2,7 +2,7 @@ function Directive($state, QuestionService){
     "ngInject";
 
     function link(scope, element, attributes){
-
+        //set up initial variables
         scope.uploaded = false;
         scope.error = false;
 
@@ -11,17 +11,27 @@ function Directive($state, QuestionService){
         };
 
         setAnswer(scope);
-
+        //Upload image upon the click of the Upload Button
         scope.uploadImage = function(file){
-            fileUpload(file, scope);
+            //reset the check variables
+            scope.uploaded = false;
+            scope.error = false;
+            if(file){
+                fileUpload(file, scope);
+            } else {
+                _scope.error = true;
+            }
         }
 
     }
 
+    //Purpose: Upload Image on the click of the Upload button
+    //Params: file - Upload Image, Scope - Object
     function fileUpload( file, scope ) {
         var _scope = scope;
         QuestionService.uploadImage(file).then(function(res){
             _scope.uploaded = true;
+            //set the model answer
             _scope.model.answer = res;
         }, function(err){
             _scope.error = true;
@@ -30,6 +40,8 @@ function Directive($state, QuestionService){
 
     }
 
+    //Purpose: Set the answer on load if answer is available
+    //Params: scope - Object
     function setAnswer( scope ){
         var _scope = scope;
         scope.$watch('savedAnswers', function(newAnswer){
