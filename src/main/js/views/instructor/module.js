16d2d2af1a6e72d6ab3ed3c24387151f1c321bfa
Nazariy995 <nazariy1995@gmail.com>
2017-03-22 13:50:05 -0400

    ADD:  min and max to subject and course code

--- src/main/js/views/instructor/module.js
+++ src/main/js/views/instructor/module.js
@@ -2,9 +2,11 @@
 var coursesAddEditController = require("./courses_add_edit/controller");
 var courseMembersController = require('./course_members/controller');
 var courseMembersAddController = require('./course_members_add/controller');
+var assignmentsAddEditController = require('./assignments_add_edit/controller');
 
 module.exports = angular.module('app.views.instructor', [
     coursesAddEditController.name,
     courseMembersController.name,
-    courseMembersAddController.name
+    courseMembersAddController.name,
+    assignmentsAddEditController.name
 ]);
--- src/main/js/views/student/module.js
+++ src/main/js/views/student/module.js
@@ -1,5 +1,5 @@
 
-var assignmentController = require("./assignments/controller");
+//var assignmentController = require("./assignments/controller");
 var assignmentDetailsController = require("./assignment/controller");
 var assignmentGroupController = require("./assignment_create_group/controller");
 var assignmentLoginController = require("./assignment_group_login/controller");
@@ -8,7 +8,7 @@ var courseController = require("./course/controller");
 var gradesController = require("./grades/controller");
 
 module.exports = angular.module('app.views.student', [
-    assignmentController.name,
+//    assignmentController.name,
     courseController.name,
     gradesController.name,
     assignmentDetailsController.name,
