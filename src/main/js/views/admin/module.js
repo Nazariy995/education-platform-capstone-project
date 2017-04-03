6b420842bb6f583f406c5607fe54575876f2dda8
Rodney Lewis <rodneyll@umich.edu>
2017-04-03 12:59:37 -0400

    Fixed .pom build error
    Controller wasn't able to found. Redirect works again.

--- src/main/js/views/admin/module.js
+++ src/main/js/views/admin/module.js
@@ -1,4 +1,4 @@
-var adminAddEditController = require("./user_add_edit");
+var adminAddEditController = require("./user_add_edit/controller");
 var adminHomeController = require("./home/controller");
 
 module.exports = angular.module('app.views.admin', [
