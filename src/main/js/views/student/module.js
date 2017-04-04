7f8a408f0bd4eba14680c2aa7b39a823b7e3041a
Nazariy995 <nazariy1995@gmail.com>
2017-04-03 20:51:39 -0400

    ADD: get grade for an individual assignment

--- src/main/js/views/student/module.js
+++ src/main/js/views/student/module.js
@@ -1,16 +1,12 @@
 
-//var assignmentController = require("./assignments/controller");
 var assignmentDetailsController = require("./assignment/controller");
 var assignmentGroupController = require("./assignment_create_group/controller");
 var assignmentLoginController = require("./assignment_group_login/controller");
 var assignmentQuestionsController = require('./assignment_questions/controller');
 var courseController = require("./course/controller");
-var gradesController = require("./grades/controller");
 
 module.exports = angular.module('app.views.student', [
-//    assignmentController.name,
     courseController.name,
-    gradesController.name,
     assignmentDetailsController.name,
     assignmentGroupController.name,
     assignmentLoginController.name,
